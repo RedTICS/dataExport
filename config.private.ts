60c7ed52949b1eedcdf4ee06ce3105682fc06bb7
NicoDino <nicodinolfo@gmail.com>
2017-08-17 10:53:25 -0300

    commit inicial

---
+++ config.private.ts
@@ -0,0 +1,8 @@
+let configPrivate = {
+    mongoDB: {
+        host: 'mongodb://10.1.62.17:27028/andes',
+        collection: 'paciente'
+    }
+}
+
+export = configPrivate;
---
+++ index.ts
@@ -0,0 +1,50 @@
+import * as configPrivate from './config.private';
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
+const url = configPrivate.mongoDB.host;
+const coleccion = configPrivate.mongoDB.collection;
+
+exportPacientes();
+
+function exportPacientes() {
+    console.log("Entrando a la funcion");
+    MongoClient.connect(url, function (err: any, dbMongo: any) {
+        if (err) {
+            console.log('Error conectando a mongoClient', err);
+            dbMongo.close();
+        }
+        dbMongo.collection(coleccion).find({}).toArray(function (err, docs) {
+            console.log("Found the following records");
+            let writer = fs.createWriteStream('prueba.txt', {
+                flags: 'a' // 'a' means appending (old data will be preserved)
+            })
+            docs.forEach(element => {
+                let sexo = (element.sexo == 'femenino') ? 'F' : 'M';
+                // Formateamos nombre de acuerdo a especificaciones Anses
+                let longNombre = element.apellido.length + element.nombre.length;
+                let nombreCompleto = element.apellido + ' ' + element.nombre;
+                for (let i = longNombre + 1; i < 40; i++) {
+                    nombreCompleto = nombreCompleto + ' ';
+                }
+
+                let doc = element.documento;
+                // formateamos la fecha "aaammdd"
+                let mes = (((element.fechaNacimiento.getMonth() + 1).toString()).length === 1) ? ('0' + (element.fechaNacimiento.getMonth() + 1).toString()) : (element.fechaNacimiento.getMonth() + 1).toString();
+                let dia = ((element.fechaNacimiento.getDate().toString()).length === 1) ? ('0' + (element.fechaNacimiento.getDate().toString())) : element.fechaNacimiento.getDate().toString();
+                let fechaNac = element.fechaNacimiento.getFullYear().toString() + mes + dia;
+                // formateamos DNI
+                let longDocumento = element.documento.length;
+                for (let i = longDocumento; i < 8; i++) {
+                    doc = '0' + doc;
+                }
+                if (longDocumento > 8) {
+                    doc = doc.substring(0, 8);
+                }
+                writer.write('29' + doc + nombreCompleto + sexo + fechaNac + '                                                                                                    ');
+                writer.write('\n');
+            });
+            dbMongo.close();
+        });
+    })
+}  
