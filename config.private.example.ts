3d8bc7fe7369c90f1679ea4abca1356269f2a3e0
Hugo <hfernandez@neuquen.gov.ar>
2018-03-08 15:03:24 -0300

    add template

---
+++ config.private.example.ts
@@ -0,0 +1,18 @@
+let configPrivate = {
+    mongoDB: {
+        host: 'mongodb://------',
+        collection: '----'
+    },
+    URL: {
+        base: '/api/core/mpi/pacientes/mpi' 
+    },
+    secret: {
+        token: '----'
+    },
+    network: {
+        host : '127.0.0.1',
+        port : 3002
+    }
+}
+
+export = configPrivate;
