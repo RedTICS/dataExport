a7f7e718153d9d5aa7cb19502584467c7903a705
Hugo <hfernandez@neuquen.gov.ar>
2018-03-08 15:02:16 -0300

    mejoras menu y export a mongodb

--- index.ts
+++ index.ts
@@ -1,120 +1,196 @@
 import * as configPrivate from './config.private';
+import {
+    resolve
+} from 'path';
+import {
+    PacienteMpi
+} from './paciente.service';
 const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
 
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
-var inquirer = require('inquirer');
+let inquirer = require('inquirer');
 
 const url = configPrivate.mongoDB.host;
 const coleccion = configPrivate.mongoDB.collection;
+const directory = 'export/PacientesCuilificados';
+
 
 inicio();
 
-
-
 function inicio() {
     clear();
+    let arrayFiles: String[] = [];
+
     console.log(
         chalk.red.bold(
-            figlet.textSync('Cuilificador', { horizontalLayout: 'full' })
+            figlet.textSync('Cuilificador', {
+                horizontalLayout: 'full'
+            })
         )
     );
 
-    const questions = [
-        {
-            name: 'cuilificar',
-            type: 'rawlist',
-            message: 'Seleccione una opción: ',
-            choices: [
-                {
-                    key: "1",
-                    name: "Importar Pacientes cuilificados",
-                    value: "importar"
-                },
-                {
-                    key: "2",
-                    name: "Exportar Pacientes para cuilificar",
-                    value: "exportar"
-                },
-                {
-                    key: "3",
-                    name: "Ninguna",
-                    value: "ninguna"
-                }
-            ],
-            validate: function (value) {
-                if (value.length) {
-                    return true;
-                } else {
-                    return 'Debe seleccionar una opción';
-                }
+    const questions = [{
+        name: 'cuilificar',
+        type: 'rawlist',
+        message: 'Seleccione una opción: ',
+        choices: [{
+                key: "1",
+                name: "Importar Pacientes cuilificados",
+                value: "importar"
+            },
+            {
+                key: "2",
+                name: "Exportar Pacientes para cuilificar de mongodb a un archivo intermedio exportPatient.txt",
+                value: "exportar"
+            },
+            {
+                key: "3",
+                name: "Ninguna",
+                value: "ninguna"
+            }
+        ],
+        validate: function (value: any) {
+            if (value.length) {
+                return true;
+            } else {
+                return 'Debe seleccionar una opción';
             }
         }
-    ];
+    }];
 
     inquirer.prompt(questions).then(answers => {
         if (answers.cuilificar === 'importar') {
-            importarPacientesCuilificados();
+            readFilesfromFS()
+                .then(function (files: any) {
+                    if (files.length) {
+                        let menu = [{
+                            name: 'archivoElegido',
+                            type: 'list',
+                            message: 'Seleccionar el archivo a procesar:',
+                            choices: files,
+                            validate: function (value: any) {
+                                if (value.length) {
+                                    return true
+                                } else {
+                                    return 'Debe seleccionar al menos un archivo';
+                                }
+                            }
+                        }]
+                        inquirer.prompt(menu)
+                            .then(fileSelected => {
+                                console.log('Inicio parseo... con archivo seleccionado: ', fileSelected.archivoElegido);
+                                importarPacientesCuilificados(fileSelected.archivoElegido)
+                                    .then(function (resultado) {
+                                        return true;
+                                    })
+                                    .catch(function (err) {
+                                        console.log('err: ', err)
+                                    })
+                            })
+                    }
+                })
+
         } else if (answers.cuilificar === 'exportar') {
             exportPacientes();
         } else {
-            console.log("No se seleccionó ninguna opción");
+            console.log("Saliendo...");
         }
     });
-}
 
-// TODO: Ver de levantar el archivo enviado por ANSES automáticamente
-function importarPacientesCuilificados() {
-    console.log("Entrando a pacientes cuilificados: ");
-
-    let archivo;
-
-    // Poner el nombre de archivo que se envió desde ANSES
-    let nombreArchivo = './pacientes_cuilificados.txt';
-
-    fs.exists(nombreArchivo, function (exists) {
-        if (exists) { // results true
-            fs.readFile(nombreArchivo, { encoding: "utf8" }, function (err, data) {
+    function readFilesfromFS() {
+        return new Promise((resolve, reject) => {
+            let count = 1;
+            let archivos: any[] = []
+            fs.readdir(directory, function (err, files) {
                 if (err) {
-                    console.log(err)
+                    return reject(err);
                 }
-                let pacientesCuilificados: any[] = data;
-                procesarDatos(pacientesCuilificados);                
+                files.forEach(f => {
+                    let elem = {
+                        key: count,
+                        name: f.toString(),
+                        value: f.toString()
+                    }
+                    count++;
+                    archivos.push(elem);
+                });
+                return resolve(archivos);
             })
-        }
-    });
+        })
+    };
+
 }
 
-function procesarDatos(data) {
-    var remaining = '';
-    let x = 1;
 
+async function importarPacientesCuilificados(arch: any) {
+    return new Promise((resolve, reject) => {
+        let pathFile = directory + '/' + arch;
+        fs.exists(pathFile, function (exists) {
+            if (exists) {
+                fs.readFile(pathFile, {
+                    encoding: "utf8"
+                }, function (err, data) {
+                    if (err) {
+                        reject(err)
+                    }
+                    let pacientesCuilificados: any[] = data;
+                    procesarDatos(pacientesCuilificados)
+                    resolve(true);
+                })
+            }
+        });
+    })
+}
+
+async function procesarDatos(data) {
+    let remaining = '';
     remaining += data;
     let index = remaining.indexOf('\n');
-
     while (index > -1) {
         let linea = remaining.substring(0, index);
+        console.log(linea);
+        await func(linea);
         remaining = remaining.substring(index + 1);
-        func(linea);
         index = remaining.indexOf('\n');
     }
-
     function func(data) {
-        let dni = data.substring(2, 10);
-        let nombreCompleto = data.substring(10, 49);
-        let sexo = data.substring(50, 51);
-        let fechaNacimiento = data.substring(51, 59);
-        let fechaFallecimiento = data.substring(59, 67);
-        let cuil = data.substring(149, 160);
-        console.log('Reg Nº ' + x + ' : ' + dni + ' - ' + nombreCompleto + ' - ' + sexo + ' - ' + fechaNacimiento + ' - ' + fechaFallecimiento + ' - ' + cuil);
-        x++;
+        return new Promise((resolve, reject) => {
+            let nombreCompleto = data.substring(10, 49);
+            let dni = data.substring(2, 10);
+            let sexo = data.substring(50, 51);
+            let cuil = data.substring(149, 160);
+            MongoClient.connect(url, function (err: any, db: any) {
+                if (err) {
+                    db.close();
+                    reject(err);
+                }
+                let query = {
+                    'documento': dni.substring(0, 1) !== '0' ? dni : dni.substring(1, 9),
+                    'sexo': (sexo === 'F') ? 'femenino' : 'masculino'
+                }
+                db.collection(coleccion).findOne(query, async function (err, pac) {
+                    if (err) {
+                        console.log('err: ', err);
+                        reject(err);
+                    }
+                    if (pac && pac.documento && !pac.cuil) {
+                        pac['cuil'] = cuil;
+                        let op = new PacienteMpi();
+                        await op.cuilificaPaciente(pac, configPrivate.secret.token);
+                    }
+                    db.close();
+                    resolve(true);
+                });
+            })
+        })
     }
 }
 
+// Exporta todos los pacientes de mongo a un archivo .txt para enviar a sips.
 function exportPacientes() {
-    console.log("Entrando a la funcion");
     MongoClient.connect(url, function (err: any, dbMongo: any) {
         if (err) {
             console.log('Error conectando a mongoClient', err);
@@ -122,7 +198,7 @@ function exportPacientes() {
         }
         dbMongo.collection(coleccion).find({}).toArray(function (err, docs) {
             console.log("Found the following records: ", docs.length);
-            let writer = fs.createWriteStream('export_sips.txt', {
+            let writer = fs.createWriteStream('exportPatient.txt', {
                 flags: 'a' // 'a' means appending (old data will be preserved)
             })
             docs.forEach(element => {
@@ -149,10 +225,10 @@ function exportPacientes() {
                 if (longDocumento > 8) {
                     doc = doc.substring(0, 8);
                 }
-                writer.write('29' + doc + nombreCompleto + sexo + fechaNac + '                                                                                                    ');
-                writer.write('\n');
+                // writer.write('29' + doc + nombreCompleto + sexo + fechaNac + '                                                                                                    ');
+                // writer.write('\n');
             });
             dbMongo.close();
         });
     })
-}  
+}
---
+++ paciente.service.ts
@@ -0,0 +1,37 @@
+import * as http from 'http';
+import * as configPrivate from './config.private';
+
+export class PacienteMpi {
+    
+   cuilificaPaciente(patient: any, token: any) {
+        return new Promise((resolve: any, reject: any) => {
+            let id = patient._id;
+            // delete paciente._id; //Borro el id para que no se duplique
+            let options = {
+                host: configPrivate.network.host,
+                port: configPrivate.network.port,
+                path: configPrivate.URL.base + '/' + id,
+                method: 'PATCH',
+                headers: {
+                    'Authorization': token,
+                    'Content-Type': 'application/json',
+                }
+            };
+            let req = http.request(options, function(res) {
+                res.on('data', function(result) {
+                    console.log('paciente actualizado: ', patient.apellido, patient.nombre, patient.documento);
+                    resolve(result);
+                });
+            });
+            req.on('error', function(e) {
+                console.log('Problemas API en update : ' + e.message + ' ----- ', e);
+                reject(e.message);
+            });
+            /*write data to request body*/
+            
+            req.write(JSON.stringify({op:'updateCuil', cuil: patient.cuil}));
+            req.end();
+        });
+
+    };
+}
