a79102e3c2edce67ea0c1f68fe6ef21429970f85
Luchito Fantasìa <luisparadawagner@gmail.com>
2018-05-09 11:32:33 -0300

    Se agrega la opción de cuilificar con mpi o sin mpi

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as configPrivate from './config.private';
 import { resolve } from 'path';
 import { PacienteMpi } from './paciente.service';
-import { PacienteSumar } from './paciente-sumar.service';
+import * as pacienteSumar from './paciente-sumar.service';
 import * as sql from 'mssql';
 import * as moment from 'moment';
 
@@ -69,40 +69,66 @@ function inicio() {
 
     inquirer.prompt(questions).then(answers => {
         if (answers.cuilificar === 'importar') {
-            readFilesfromFS()
-                .then(function (files: any) {
-                    if (files.length) {
-                        let menu = [{
-                            name: 'archivoElegido',
-                            type: 'list',
-                            message: 'Seleccionar el archivo a procesar:',
-                            choices: files,
-                            validate: function (value: any) {
-                                if (value.length) {
-                                    return true
-                                } else {
-                                    return 'Debe seleccionar al menos un archivo';
+
+            let opcion = [{
+                name: 'pacienteMPI',
+                type: 'list',
+                message: 'Seleccione cuilificación con MPI o sin MPI: ',
+                choices: [
+                    {
+                        name: 'Con MPI', value: 1
+                    },
+                    {
+                        name: 'Sin MPI', value: 2
+                    }]
+            }]
+
+            inquirer.prompt(opcion).then(answers => {
+                console.log("Respuesta", answers)
+                let mpi;
+                if (answers.pacienteMpi === 1) {
+                    mpi = true;
+                } else {
+                    mpi = false;
+                }
+
+                readFilesfromFS()
+                    .then(function (files: any) {
+                        if (files.length) {
+                            let menu = [{
+                                name: 'archivoElegido',
+                                type: 'list',
+                                message: 'Seleccionar el archivo a procesar:',
+                                choices: files,
+                                validate: function (value: any) {
+                                    if (value.length) {
+                                        return true
+                                    } else {
+                                        return 'Debe seleccionar al menos un archivo';
+                                    }
                                 }
-                            }
-                        }]
-                        inquirer.prompt(menu)
-                            .then(fileSelected => {
-                                console.log('Inicio parseo... con archivo seleccionado: ', fileSelected.archivoElegido);
-                                importarPacientesCuilificados(fileSelected.archivoElegido)
-                                    .then(function (resultado) {
-                                        return true;
-                                    })
-                                    .catch(function (err) {
-                                        console.log('err: ', err)
-                                    })
-                            })
-                    }
-                })
+                            }]
+                            inquirer.prompt(menu)
+                                .then(fileSelected => {
+                                    console.log('Inicio parseo... con archivo seleccionado: ', fileSelected.archivoElegido);
+                                    importarPacientesCuilificados(fileSelected.archivoElegido, mpi)
+                                        .then(function (resultado) {
+                                            return true;
+                                        })
+                                        .catch(function (err) {
+                                            console.log('err: ', err)
+                                        })
+                                })
+                        }
+                    })
+            });
+
+
 
         } else if (answers.cuilificar === 'exportar') {
             exportPacientes();
         } else if (answers.cuilificar === 'exportar_anses') {
-            exportarPacientesAnses();
+            pacienteSumar.exportarPacientesAnses();
         } else {
             console.log("Saliendo...");
         }
@@ -129,11 +155,9 @@ function inicio() {
             })
         })
     };
-
 }
 
-
-async function importarPacientesCuilificados(arch: any) {
+async function importarPacientesCuilificados(arch: any, mpi: boolean) {
     return new Promise((resolve, reject) => {
         let pathFile = directory + '/' + arch;
         fs.exists(pathFile, function (exists) {
@@ -145,9 +169,13 @@ async function importarPacientesCuilificados(arch: any) {
                         reject(err)
                     }
                     let pacientesCuilificados: any[] = data;
-                    // procesarDatos(pacientesCuilificados)
 
-                    guardaSipsCuilificado(pacientesCuilificados);
+                    if (mpi) {
+                        procesarDatos(pacientesCuilificados)
+                    } else {
+                        guardaSipsCuilificado(pacientesCuilificados);
+                    }
+
                     resolve(true);
                 })
             }
@@ -162,21 +190,21 @@ async function guardaSipsCuilificado(data) {
     let index = remaining.indexOf('\n');
     let x = 0;
     let pacCuil: any = [];
-    let pepe: any;
+    let paciente: any;
 
     while (index > -1) {
         let linea = remaining.substring(0, index);
         console.log(linea);
         x++;
         console.log("Total: ", x)
-        // for (let i = 0; i < data.length; i++) {
+
         let nombreCompleto = linea.substring(10, 49);
         let dni = linea.substring(2, 10);
         let sexo = linea.substring(50, 51);
         let cuil = linea.substring(149, 160);
         let fechaNacimiento = linea.substring(51, 59);
 
-        pepe = {
+        paciente = {
             nombreCompleto: nombreCompleto,
             documento: dni,
             sexo: sexo,
@@ -184,28 +212,22 @@ async function guardaSipsCuilificado(data) {
             fechaNacimiento: fechaNacimiento
         }
 
-        // pacCuil.push(pepe)
-        pacCuil.push(pepe);
+        pacCuil.push(paciente);
 
         remaining = remaining.substring(index + 1);
         index = remaining.indexOf('\n');
     }
-    // var entries = [];
 
     var total_entries = pacCuil.length;
 
     MongoClient.connect(url, function (err, db) {
-        // Get the collection
-        var col = db.collection('sipsCuilificado');
-        //
+        var col = db.collection(coleccion);
 
         var bulk = col.initializeOrderedBulkOp();
         var counter = 0;
 
         for (let i = 0; i < pacCuil.length; i++) {
             bulk.insert(pacCuil[i]);
-
-
         }
 
         bulk.execute(function (err, result) {
@@ -213,44 +235,9 @@ async function guardaSipsCuilificado(data) {
             console.dir(result);
             db.close();
         });
-
-
-        // async.whilst(
-        //     // Iterator condition
-        //     function () { return counter <= total_entries },
-
-        //     // Do this in the iterator
-        //     function (callback) {
-        //         counter++;
-
-        //         bulk.insert(pacCuil[counter]);
-
-        //         // console.log("Insertado: ", pacCuil[counter]);
-
-        //         // if (counter % 1000 == 0) {
-        //             bulk.execute(function (err, result) {
-        //                 console.log("Entrando a cero");
-        //                 bulk = col.initializeOrderedBulkOp();
-        //                 callback(err);
-        //             });
-        //         // // } else {
-        //         //     callback();
-        //         // }
-        //     },
-
-        //     // When all is done
-        //     function (err) {
-        //         // if (counter % 1000 != 0)
-        //             bulk.execute(function (err, result) {
-        //                 console.log("inserted some more");
-        //             });
-        //         console.log("I'm finished now");
-        //         db.close();
-        //     }
-        // );
     });
 
-    console.log("Cat pepe: ", pacCuil.length);
+    console.log("Cant Pac.: ", pacCuil.length);
 }
 
 async function procesarDatos(data) {
@@ -335,200 +322,12 @@ function exportPacientes() {
                 if (longDocumento > 8) {
                     doc = doc.substring(0, 8);
                 }
-                // writer.write('29' + doc + nombreCompleto + sexo + fechaNac + '                                                                                                    ');
-                // writer.write('\n');
+                
+                writer.write('29' + doc + nombreCompleto + sexo + fechaNac + '                                                                                                    ');
+                writer.write('\n');
             });
             dbMongo.close();
         });
     })
-}
-
-/* Exportar pacientes a ANSES*/
-/* TODO: Tarda demasiado, probar de hacer un SP en SQL Server*/
-async function exportarPacientesAnses() {
-    const query_limit = 1000000;
-
-    var connection = {
-        user: configPrivate.authSql.user,
-        password: configPrivate.authSql.password,
-        server: configPrivate.serverSql.server,
-        database: configPrivate.serverSql.database,
-        requestTimeout: 1900000,
-        stream: true
-    };
-
-    let pool = await new sql.ConnectionPool(connection).connect();
-
-
-
-    MongoClient.connect(url, async function (err: any, dbMongo: any) {
-        if (err) {
-            console.log('Error conectando a mongoClient', err);
-            dbMongo.close();
-        }
-        console.log("Exportando a ANSES...")
-        let cursor = dbMongo.collection('sipsCuilificado').aggregate([
-            {
-                $match: { cuil: { $ne: '', $exists: true } }
-            },
-            { $project: { cuil: 1, documento: 1, fechaNacimiento: 1, nombreCompleto: 1 } },
-            {
-                $limit: query_limit
-            }
-
-        ], {
-                cursor: {
-                    batchSize: 1
-                }
-            });
-
-        let cursorArray = cursor.toArray();
-        let x = 0;
-        let i = 1000;
-        let total = 0;
-
-        let menores = 0;
-        let cantPrestaciones = 0;
-
-        let listaPacienteAnses: any = [];
-
-        await cursorArray.then(async pacientesCuil => {
-            console.log("Paccc: ", pacientesCuil.length)
-
-            while (x < pacientesCuil.length) {
-
-                let arrayCuil = pacientesCuil.slice(x, x + i);
-
-                let prestaciones: any = await getNomivacExcel(arrayCuil, pool);
-                prestaciones = prestaciones.recordset;
-
-                cantPrestaciones = cantPrestaciones + prestaciones.length;
-                // console.log("Capoooooo: ", prestaciones);
-                // break;
-                for (let q = 0; q < prestaciones.length; q++) {
-                    let edad = await getEdadPaciente(pacientesCuil[q].fechaNacimiento);                    
-
-                    if (edad < 18) {
-                        menores++;
-                        let pacienteAnses: any = {};
-                        pacienteAnses['cuil'] = pacientesCuil[q].cuil;
-                        pacienteAnses['cuie'] = prestaciones[q].Cuie;
-                        pacienteAnses['establecimiento'] = prestaciones[q].Efector;
-                        pacienteAnses['fechaControl'] = prestaciones[q].Fecha;
-                        pacienteAnses['discapacitado'] = '  ';
-                        pacienteAnses['esquema'] = 'EN';
-                        pacienteAnses['codigoEstablecimiento'] = (prestaciones[q].CodigoEstablecimiento) ? prestaciones[q].CodigoEstablecimiento : ''; //(vacunas) ? vacunas[0].CodEstablecimiento : '';
-                        pacienteAnses['Establecimiento'] = (prestaciones[q].Establecimiento) ? prestaciones[q].Establecimiento : ' ';// (vacunas) ? vacunas[0].NombreEstablecimiento : '';
-                        pacienteAnses['fechaAplicacion'] = (prestaciones[q].fechaAplicacion) ? prestaciones[q].fechaAplicacion : '';//(vacunas) ? vacunas[0].FechaAplicacion : '';
-                        pacienteAnses['dependencia'] = '        ';
-                        pacienteAnses['sumar'] = prestaciones[q].sumar;
-
-                        listaPacienteAnses.push(pacienteAnses);
-                    }
-                    // console.log("Lista de Pacientes: ", pacienteAnses)
-                }
-
-                // listaPacienteAnses.push(pacienteAnses);
-
-                x = x + i;
-            }
-
-            pool.close();
-            // console.log("Encontrado: ", listaPacienteAnses)
-        });
-
-        await procesarDatosAnses(listaPacienteAnses);
-        console.log("Menores: ", menores);
-        console.log("Cant de Prestaciones: ", cantPrestaciones);
-    });
-}
-
-async function procesarDatosAnses(data) {
-
-    let writer = fs.createWriteStream('pacientes_anses.txt', {
-        flags: 'a' // 'a' means appending (old data will be preserved)
-    });
-
-    data.forEach(x => {        
-        let cuil = x.cuil;
-        let sumar = x.sumar;
-        let sisa = ' '.repeat(15);
-        let cuie = x.cuie + ' '.repeat(9 - x.cuie.length);
-        let establecimiento = (x.establecimiento.length <= 35) ? x.establecimiento + ' '.repeat(35 - x.establecimiento.length) : x.establecimiento.substring(0, 35);
-        let discapacitado = x.discapacitado;
-        let fechaControl = moment(x.fechaControl).format("YYYYMMDD");
-        let esquema = x.esquema;
-        let codigoEstablecimiento = (x.codigoEstablecimiento) ? x.codigoEstablecimiento.substring(0, 9) : '';
-        let nombreEstablecimiento = (x.Establecimiento.length <= 35) ? x.Establecimiento + ' '.repeat(35 - x.Establecimiento.length) : x.Establecimiento.substring(0, 35);
-        let fechaAplicacion = (x.fechaAplicacion) ? moment(x.fechaAplicacion).format("YYYYMMDD") : '';
-        let dependencia = x.dependencia;
-        let relleno = ' '.repeat(156);
-
-        writer.write(cuil + sumar + sisa + cuie + establecimiento + discapacitado + fechaControl + esquema + codigoEstablecimiento + nombreEstablecimiento + fechaAplicacion + dependencia + relleno);
-        writer.write('\n');
-    });
-}
-
-// async function getDatosSumar(dni, pool) {
-//     return await pool.request()
-//         .input('dni', sql.VarChar(50), dni)
-//         .input('activo', sql.Char, 'S')
-//         .input('aficlasedoc', sql.Char, 'P')
-//         .query('SELECT * FROM dbo.PN_smiafiliados WHERE afidni = @dni and activo = @activo AND aficlasedoc = @aficlasedoc');
-// }
-
-// async function getPrestacion(dni, pool) {
-//     return await pool.request()
-//         .input('dni', sql.VarChar(50), dni)
-//         .query('SELECT e.nombre as Efector, c.fecha as Fecha, p.fechaNacimiento as FechaNacimiento, e.cuie as Cuie FROM dbo.CON_ConsultaDiagnostico cd '
-//             + 'INNER JOIN dbo.CON_Consulta c ON c.idConsulta = cd.idConsulta '
-//             + 'INNER JOIN dbo.Sys_Efector e ON e.idEfector = c.idEfector '
-//             + 'INNER JOIN dbo.Sys_Paciente p ON p.idPaciente = c.idPaciente '
-//             + 'WHERE p.numeroDocumento = @dni and cd.CODCIE10 IN (9448, 9449, 9450, 9451)');
-// }
-
-// async function getNomivac(dni, pool) {
-//     return await pool.request()
-//         .input('dni', sql.VarChar(50), dni)
-//         .query('SELECT [Código de establecimiento] as CodEstablecimiento,Establecimiento as NombreEstablecimiento, [Fecha de aplicación] as FechaAplicacion FROM dbo.NomivacExcel where [Nro# de documento] = @dni')
-// }
-
-
-async function getNomivacExcel(pacientesCuil, pool) {
-
-    let sQuery = " SELECT  p.numeroDocumento, n.Establecimiento, n.[Código de establecimiento]AS CodigoEstablecimiento, n.[Fecha de aplicación] AS fechaAplicacion, p.nombre, p.apellido, e.nombre as Efector, c.fecha as Fecha, p.fechaNacimiento as FechaNacimiento, e.cuie as Cuie, "
-        + " CASE WHEN a.activo = 'S' AND a.aficlasedoc = 'P' "
-        + "          THEN 'SI' "
-        + "       ELSE 'NO' "
-        + " END AS sumar "
-        + " FROM dbo.CON_ConsultaDiagnostico cd "
-        + " INNER JOIN dbo.CON_Consulta c ON c.idConsulta = cd.idConsulta "
-        + " INNER JOIN dbo.Sys_Efector e ON e.idEfector = c.idEfector "
-        + "INNER JOIN dbo.Sys_Paciente p ON p.idPaciente = c.idPaciente  "
-        + "LEFT JOIN dbo.NomivacExcel n ON n.[Nro# de documento] = p.numeroDocumento "
-        + " LEFT JOIN dbo.PN_smiafiliados a ON a.afidni = CONVERT(VARCHAR(10), p.numeroDocumento) "
-        + " WHERE p.numeroDocumento in (";
-    for (let p = 0; p < pacientesCuil.length; p++) {
-        // sQuery += "'" + pacientesCuil[p].documento + "',";
-        sQuery += "'48969032',";
-    }
-    sQuery = sQuery.substring(0, sQuery.length - 1, );
-    sQuery += ") and  cd.CODCIE10 IN (9448, 9449, 9450, 9451) ";
-
-    // console.log("Queryyy: ", sQuery);
-    return await pool.request()
-        .query(sQuery);
-}
-
-// async function getPrestaciones(dni, pool) {
-//     return await pool.request()
-//         .input('dni', sql.VarChar(50), dni)
-//         .execute('SP_Cuilificador')
-// }
-
-function getEdadPaciente(fechaNacimiento: string): number {    
-    let edad = moment().diff(fechaNacimiento, 'years');
-    
-    return edad;
 }
 
--- paciente-sumar.service.ts
+++ paciente-sumar.service.ts
@@ -2,42 +2,159 @@ import * as http from 'http';
 import * as sql from 'mssql';
 import * as configPrivate from './config.private';
 
-export class PacienteSumar {
+import * as moment from 'moment';
 
-    async  isPacienteSumar(dni: any, pool) {
-        console.log("Dni Paciente Sumar: ", dni)       
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
 
-        var listaRegistros: any[] = [];
+const url = configPrivate.mongoDB.host;
 
-        // return new Promise(async (resolve: any, reject: any) => {
-    
-        let query = "SELECT * FROM dbo.PN_smiafiliados WHERE afidni = '" + dni + "'";
-        console.log("Entra a promise; ", query)
-        // let capo = await executeQuery(query, pool);
-        
-        // (async function () {
-        
-        // try {
-        //     // let pool = await sql.connect(connection)
-        //     console.log("Entra aal Pooll: ")
-        //     let result1 =await new sql.Request(pool)
-        //         .input('input_parameter', sql.VarChar(50), dni)
-        //         .query('SELECT * FROM dbo.PN_smiafiliados WHERE afidni = @input_parameter')
+/* Exportar pacientes a ANSES*/
+/* TODO: Tarda demasiado, probar de hacer un SP en SQL Server*/
+export async function exportarPacientesAnses() {
+    const query_limit = 1000000;
 
-        //     console.log("Eneuntra paciente: ", result1)
-        //     pepe = result1;
+    var connection = {
+        user: configPrivate.authSql.user,
+        password: configPrivate.authSql.password,
+        server: configPrivate.serverSql.server,
+        database: configPrivate.serverSql.database,
+        requestTimeout: 1900000,
+        stream: true
+    };
 
-        //     // resolve(pepe)
-        // } catch (err) {
-        //     console.log("Catchhhhhh: ", err)
-        //     // reject(err)
+    const coleccion = configPrivate.mongoDB.collection;
+    let pool = await new sql.ConnectionPool(connection).connect();
 
-        //     // ... error checks
-        // }
-        // });
-        // resolve(pepe)
-        // });
+    MongoClient.connect(url, async function (err: any, dbMongo: any) {
+        if (err) {
+            console.log('Error conectando a mongoClient', err);
+            dbMongo.close();
+        }
+        console.log("Exportando a ANSES...")
+        let cursor = dbMongo.collection(coleccion).aggregate([
+            {
+                $match: { cuil: { $ne: '', $exists: true } }
+            },
+            { $project: { cuil: 1, documento: 1, fechaNacimiento: 1, nombreCompleto: 1 } },
+            {
+                $limit: query_limit
+            }
+        ], {
+                cursor: {
+                    batchSize: 1
+                }
+            });
 
-    }
+        let cursorArray = cursor.toArray();
+        let x = 0;
+        let i = 100;
+        let total = 0;
+
+        let menores = 0;
+        let mayores = 0;
+        let cantPrestaciones = 0;
+
+        let listaPacienteAnses: any = [];
+        let listaDni: any = [];
+
+        await cursorArray.then(async pacientesCuil => {
+            console.log("Cant Pacientes: ", pacientesCuil.length)
+
+            for (let z = 0; z < pacientesCuil.length; z++) {
+
+                let fechaNac = (pacientesCuil[z].fechaNacimiento) ? pacientesCuil[z].fechaNacimiento : '';
+
+                let edad = await getEdadPaciente(fechaNac);
+                listaDni.push(pacientesCuil[z].documento);
+                
+                if (edad <= 18) {
+                    menores++;
+
+                    let prestaciones: any = await getNomivacExcel(pacientesCuil[z], pool);
+                    prestaciones = prestaciones.recordset;
+
+                    if (prestaciones.length > 0) {
+                        let pacienteAnses: any = {};
+
+                        pacienteAnses['cuil'] = pacientesCuil[z].cuil;
+                        pacienteAnses['documento'] = pacientesCuil[z].documento;
+                        pacienteAnses['cuie'] = prestaciones[0].Cuie;
+                        pacienteAnses['establecimiento'] = prestaciones[0].Efector;
+                        pacienteAnses['fechaControl'] = prestaciones[0].Fecha;
+                        pacienteAnses['discapacitado'] = '  ';
+                        pacienteAnses['esquema'] = 'EN';
+                        pacienteAnses['codigoEstablecimiento'] = (prestaciones[0].CodigoEstablecimiento) ? prestaciones[0].CodigoEstablecimiento : ''; //(vacunas) ? vacunas[0].CodEstablecimiento : '';
+                        pacienteAnses['Establecimiento'] = (prestaciones[0].Establecimiento) ? prestaciones[0].Establecimiento : ' ';// (vacunas) ? vacunas[0].NombreEstablecimiento : '';
+                        pacienteAnses['fechaAplicacion'] = (prestaciones[0].fechaAplicacion) ? prestaciones[0].fechaAplicacion : '';//(vacunas) ? vacunas[0].FechaAplicacion : '';
+                        pacienteAnses['dependencia'] = '        ';
+                        pacienteAnses['sumar'] = prestaciones[0].sumar;
+                        
+                        listaPacienteAnses.push(pacienteAnses);                        
+                    }
+                } else {
+                    mayores++;
+                }
+            }
+        });
+
+        await procesarDatosAnses(listaPacienteAnses);
+        pool.close();
+
+        console.log("Menores: ", menores);
+        console.log("Mayores: ", mayores);
+
+    });
 }
 
+async function procesarDatosAnses(data) {
+    let writer = fs.createWriteStream('pacientes_anses.txt', {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+    });
+
+    data.forEach(x => {
+        let cuil = x.cuil;
+        let documento = x.documento;
+        let sumar = x.sumar;
+        let sisa = ' '.repeat(15);
+        let cuie = x.cuie + ' '.repeat(9 - x.cuie.length);
+        let establecimiento = (x.establecimiento.length <= 35) ? x.establecimiento + ' '.repeat(35 - x.establecimiento.length) : x.establecimiento.substring(0, 35);
+        let discapacitado = x.discapacitado;
+        let fechaControl = moment(x.fechaControl).format("YYYYMMDD");
+        let esquema = x.esquema;
+        let codigoEstablecimiento = (x.codigoEstablecimiento) ? x.codigoEstablecimiento.substring(0, 9) : '';
+        let nombreEstablecimiento = (x.Establecimiento.length <= 35) ? x.Establecimiento + ' '.repeat(35 - x.Establecimiento.length) : x.Establecimiento.substring(0, 35);
+        let fechaAplicacion = (x.fechaAplicacion) ? moment(x.fechaAplicacion).format("YYYYMMDD") : '';
+        let dependencia = x.dependencia;
+        let relleno = ' '.repeat(156);
+
+        writer.write(cuil + sumar + sisa + cuie + establecimiento + discapacitado + fechaControl + esquema + codigoEstablecimiento + nombreEstablecimiento + fechaAplicacion + dependencia + relleno);
+        writer.write('\n');
+    });
+}
+
+async function getNomivacExcel(pacientesCuil, pool) {
+    let sQuery = " SELECT TOP 1 p.numeroDocumento, n.Establecimiento, n.[Código de establecimiento]AS CodigoEstablecimiento, n.[Fecha de aplicación] AS fechaAplicacion, p.nombre, p.apellido, e.nombre as Efector, c.fecha as Fecha, p.fechaNacimiento as FechaNacimiento, e.cuie as Cuie, "
+        + " CASE WHEN a.activo = 'S' AND a.aficlasedoc = 'P' "
+        + "          THEN 'SI' "
+        + "       ELSE 'NO' "
+        + " END AS sumar "
+        + " FROM dbo.Sys_Paciente p "
+        + " LEFT JOIN dbo.CON_Consulta c ON c.idPaciente = p.idPaciente "
+        + " LEFT JOIN dbo.NomivacExcel n ON n.[Nro# de documento] = p.numeroDocumento "
+        + " LEFT JOIN dbo.PN_smiafiliados a ON a.afidni = CONVERT(VARCHAR(10), p.numeroDocumento) "
+        + " LEFT JOIN dbo.PN_comprobante com ON com.id_smiafiliados = a.id_smiafiliados "
+        + " INNER JOIN dbo.Sys_Efector e ON e.idEfector = c.idEfector "
+        + " WHERE p.numeroDocumento = '" + pacientesCuil.documento + "'";
+
+    return await pool.request()
+        .query(sQuery);
+}
+
+async function getEdadPaciente(fechaNacimiento: string) {
+    return new Promise((resolve: any, reject: any) => {
+        let edad = moment().diff(fechaNacimiento, 'years');
+
+        resolve(edad)
+    });
+}
